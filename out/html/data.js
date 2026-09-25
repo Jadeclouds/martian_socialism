232970e32caddd380e4354d5d6b0452b4613fdf2
JadeClouds <blablachan0@gmail.com>
2026-09-25 11:10:10 +0200

    Create data.js with tooltip and colour configurations
    Added tooltip and colour lists for political parties.

---
+++ out/html/data.js
@@ -0,0 +1,26 @@
+const tooltipList = [
+  {
+    searchString: ["SPM", "spm"],
+    mainText: "Socialist Party of Mars",
+    subText: "Socialist and Democratic",
+    img: "img/parties/logo_spm.svg",
+    ledBy: "spm_leader",       // Pulls $Q.spm_leader dynamically from Dendry state
+    ideology: "spm_ideology",   // Pulls $Q.spm_ideology dynamically from Dendry state
+  },
+  {
+    searchString: ["MNLP", "mnlp"],
+    mainText: "Martian National Liberal Party",
+    img: "img/parties/logo_mnlp.svg",
+    ledBy: "mnlp_leader",
+    ideology: "mnlp_ideology",
+  }
+];
+
+const colourList = [
+  { words: ["SPM", "spm"], colour: "#ED7158", style: "font-weight: bold;", transform: "SPM" },
+  { words: ["MNLP", "mnlp"], colour: "#D3C24D", style: "font-weight: bold;", transform: "MNLP" },
+  { words: ["PLFM", "plfm"], colour: "#540808", style: "font-weight: bold;", transform: "PLFM" },
+  { words: ["FfM", "ffm"], colour: "#5C3F1A", style: "font-weight: bold;", transform: "FfM" },
+  { words: ["Phobos"], colour: "#12DB95", style: "font-weight: bold;", transform: "Phobos" },
+  { words: ["PEC", "pec"], colour: "#22275C", style: "font-weight: bold;", transform: "PEC" },
+];
