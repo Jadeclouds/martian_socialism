092ba954435eb9f04b7105d164eb6a2200a6b243
JadeClouds <blablachan0@gmail.com>
2026-09-25 23:58:30 +0200

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,36 +1,36 @@
 const tooltipList = [
   {
-    searchString: ["SPM", "spm"],
+    searchString: ["spm"],
     mainText: "Socialist Party of Mars",
     subText: "This is us: a democratic socialist party representing the Martian working class. Born out of the earliest labor struggles on Mars, we have long been the most popular socialist party on the Red Planet.",
     img: "img/spm_logo.png",
   },
   {
-    searchString: ["MNLP", "mnlp"],
+    searchString: ["mnlp"],
     mainText: "Martian National Liberal Party",
-    subText: "A big-tent center-left to center-right party formed soon after the establishment of the Martian Parliament to represent the interests of Mars's middle and upper classes, as well as moderate independentists.",
+    subText: "A big-tent center-left to center-right party formed soon after the establishment of the Martian Parliament to represent the interests of the Marsian middle and upper classes, as well as moderate independentists.",
     img: "img/mnlp_logo.png",
   },
   {
-    searchString: ["PLFM", "plfm"],
+    searchString: ["plfm"],
     mainText: "People's Liberation Front for Mars",
     subText: "The PLFM is the political wing of a broader constellation of radical left militants, drawing its main support from Martian miners. Ranging from moderate cells to uncompromising radicals, the PLFM generally adheres to MLM principles and rallies around Mao Zedong's 'On Guerrilla Warfare'.",
     img: "img/plfm_logo.png",
   },
   {
-    searchString: ["FfM", "ffm"],
+    searchString: ["ffm"],
     mainText: "Front for Mars",
     subText: "Born from a split within the PLFM, the Front for Mars is a national-syndicalist party that claims adherence to the principles of Sansepolcrismo and Sorelianism.",
     img: "img/ffm_logo.png",
   },
   {
-    searchString: ["P", "p"],
+    searchString: ["p"],
     mainText: "Phobos",
     subText: "An accelerationist party with both left-wing and right-wing factions. While relatively strong among the Martian upper class, the party lacks significant allies and currently finds itself isolated from mainstream politics.",
     img: "img/p_logo.png",
   },
   {
-    searchString: ["PEC", "pec"],
+    searchString: ["pec"],
     mainText: "Pro-Earth Camp",
     subText: "Less a traditional party and more an extension of Earth's power on Mars, the PEC has governed since the establishment of the Martian Parliament, primarily protecting the interests of Earth-aligned business owners and space conglomerates.",
     img: "img/pec_logo.png",
